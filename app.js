eac8c16056a77a8b2d9d81ef1f87c78d4692da5c
amboz <abozigian@gmail.com>
2017-10-25 12:37:05 -0700

    Still need to assign unique keys to items

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <h1>Groceries To Buy</h1>
+    <GroceryListItem stuffToBuy={['omg', 'this works?', 'cool']}/>
+  </div>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    })
+    console.log('I got clicked!!!');
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <ul>
+      {
+        this.props.stuffToBuy.map((item) => {
+          return <li style={style} onClick={this.onListItemClick.bind(this)}>{item}</li>
+        })
+      }
+      </ul>
+    );
+  }
+}
+
+// = (props) => {
+//   var onListItemClick = (event) => {
+//     console.log('I got clicked!!!');
+//   };
+
+//   return (
+//     <ul>
+//     {
+//       props.stuffToBuy.map(function(item) {
+//         return <li onClick={onListItemClick}>{item}</li>
+//       })
+//     }
+//   </ul>
+//   )
+// };
+
+// var Stuff = () => (
+//   <div>
+//     <li>stuff</li>
+//   </div>
+// );
+
+// var MoreStuff = () => (
+//   <li>moar stuff</li>
+// );
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
